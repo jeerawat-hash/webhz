934521c93c785cbb8669924fc5be2d0d7e1e63d7
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-14 23:41:44 +0700

    add

--- 3rd_party/messageapi/call.php
+++ 3rd_party/messageapi/call.php
@@ -52,125 +52,24 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 
 
 
-$jsonFlex = '[
-  "type": "flex",
-  "altText": "Hello Flex Message",  
-  "contents": [
-    "type": "bubble",
-    "direction": "ltr",
-    "header": [
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        [
-          "type": "text",
-          "text": "Purchase",
-          "size": "lg",
-          "align": "start",
-          "weight": "bold",
-          "color": "#009813"
-        ],
-        [
-          "type": "text",
-          "text": "฿ 100.00",
-          "size": "3xl",
-          "weight": "bold",
-          "color": "#000000"
-        ],
-        [
-          "type": "text",
-          "text": "Rabbit Line Pay",
-          "size": "lg",
-          "weight": "bold",
-          "color": "#000000"
-        ],
-        [
-          "type": "text",
-          "text": "2019.02.14 21:47 (GMT+0700)",
-          "size": "xs",
-          "color": "#B2B2B2"
-        ],
-        [
-          "type": "text",
-          "text": "Payment complete.",
-          "margin": "lg",
-          "size": "lg",
-          "color": "#000000"
-        ]
-      ]
-    ],
-    "body": [
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        [
-          "type": "separator",
-          "color": "#C3C3C3"
-        ],
-        [
-          "type": "box",
-          "layout": "baseline",
-          "margin": "lg",
-          "contents": [
-            [
-              "type": "text",
-              "text": "Merchant",
-              "align": "start",
-              "color": "#C3C3C3"
-            ],
-            [
-              "type": "text",
-              "text": "BTS 01",
-              "align": "end",
-              "color": "#000000"
-            ]
-          ]
-        ],
-        [
-          "type": "box",
-          "layout": "baseline",
-          "margin": "lg",
-          "contents": [
-            [
-              "type": "text",
-              "text": "New balance",
-              "color": "#C3C3C3"
-            ],
-            [
-              "type": "text",
-              "text": "฿ 45.57",
-              "align": "end"
-            ]
-          ]
-        ],
-        [
-          "type": "separator",
-          "margin": "lg",
-          "color": "#C3C3C3"
-        ]
-      ]
-    ],
-    "footer": [
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        [
-          "type": "text",
-          "text": "View Details",
-          "size": "lg",
-          "align": "start",
-          "color": "#0084B6",
-          "action": [
-            "type": "uri",
-            "label": "View Details",
-            "uri": "https://google.co.th/"
-          ]
-        ]
-      ]
+$jsonFlex = '{
+  "type": "bubble",
+  "direction": "ltr",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "อยู่ระหว่างพัฒนา..........",
+        "weight": "bold",
+        "size": "xl",
+        "align": "center",
+        "contents": []
+      }
     ]
-  ]
-]
-';
+  }
+}';
 
 
 
@@ -217,7 +116,7 @@ if (!is_null($events['events'])) {
 
                 $data = [
                     'replyToken' => $replyToken,
-                    'messages' => [$jsonFlex]
+                    'messages' => $jsonFlex
                 ];
  
                   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
