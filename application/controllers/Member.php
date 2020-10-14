5e49f22a95777d8d857694cb1d69eccd205458a5
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-15 00:44:07 +0700

    addd

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -19,39 +19,7 @@ class Member extends CI_Controller {
  	public function SendRegister()
  	{
  		
-				$value = array( "ID" => $_POST["Username"],
-							 	"Password" => $_POST["Password"],
-							 	"LineID" => $_POST["LineID"] );
-				//API URL
-				$url = "http://103.82.249.205:1234/api/register.php";
-
-				//create a new cURL resource
-				$ch = curl_init($url);
-
-				//setup request to send json via POST
-				//$data = $array;
-
-				$payload = json_encode($value);
-
-				//attach encoded JSON string to the POST fields
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
-				//set the content type to application/json
-				curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-
-				//return response instead of outputting
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-				//execute the POST request
-				 $result = curl_exec($ch); 
-
-				//close cURL resource
-				curl_close($ch);
-
-				echo $result;
-		
-
-			//print_r($_POST);
+				
 
 
  	}
--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -130,7 +130,7 @@
 					//swal("ผิดพลาดรหัสซ้ำซ้อน !!","กรุณาระบุข้อมูลใหม่", "error");
 					//swal("บันทึกข้อมูลสำเร็จ !!","ระบบกำลังปิดหน้าลงทะเบียน....", "success");
 				   
-				   	$.post("https://rhz.webclient.me/index.php/member/SendRegister",{
+				   	$.post("./3rd_party/api/register_api.php",{
 				   		Username : User,
 				   		Password : Pass,
 				   		LineID : Line
