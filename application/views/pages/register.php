503916784764e8f9ac8630faf3021bd387047908
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-14 21:09:48 +0700

    add

--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -7,7 +7,8 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="https://rhz.webclient.me/assets/Login/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
