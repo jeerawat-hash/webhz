a3aacb9e6d3ec07d3a60950fd12e6e5a5f37eb16
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-14 21:02:03 +0700

    add

--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -5,26 +5,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="https://rhz.webclient.me/assets/login/images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="https://rhz.webclient.me/assets/Login/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/vendor/select2/select2.min.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/css/util.css">
-	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/login/css/main.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="https://rhz.webclient.me/assets/Login/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -79,21 +79,21 @@
 
 	
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/login/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/login/vendor/animsition/js/animsition.min.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/login/vendor/bootstrap/js/popper.js"></script>
-	<script src="https://rhz.webclient.me/assets/login/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/bootstrap/js/popper.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/login/vendor/select2/select2.min.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/login/vendor/daterangepicker/moment.min.js"></script>
-	<script src="https://rhz.webclient.me/assets/login/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/daterangepicker/moment.min.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/login/vendor/countdowntime/countdowntime.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/login/js/main.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/js/main.js"></script>
 
 </body>
 </html>
