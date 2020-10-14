fd9788fc30f8ee27cb44d01ae91f323c53163f68
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-14 21:24:39 +0700

    add

--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -72,11 +72,15 @@
 <script type="text/javascript">
 	
 
-//	$("#preload").hide();
-//	$("#Submit").show();
+	$(function(){
+ 
+
+ 
+	$("#preload").hide();
+	$("#Submit").show();
 
 	$("#Submit").on("click",function(){
-		/*
+	
 		$("#Submit").hide();
 		$("#preload").show();
 		setTimeout(function(){ 
@@ -85,14 +89,14 @@
 			$("#preload").hide();
 
 		}, 3000);
-	*/
-	alert();
-
 
 	});
 
 
 
+
+	});
+ 
 
 </script>
 
@@ -101,14 +105,12 @@
 
 	
 <!--===============================================================================================-->
-	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
 	<script src="https://rhz.webclient.me/assets/Login/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-	<script src="https://rhz.webclient.me/assets/Login/vendor/bootstrap/js/popper.js"></script> 
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+	<script src="https://rhz.webclient.me/assets/Login/vendor/bootstrap/js/popper.js"></script>
+	<script src="https://rhz.webclient.me/assets/Login/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
 	<script src="https://rhz.webclient.me/assets/Login/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
@@ -117,7 +119,8 @@
 <!--===============================================================================================-->
 	<script src="https://rhz.webclient.me/assets/Login/vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
- 
+	<script src="https://rhz.webclient.me/assets/Login/js/main.js"></script>
+
 </body>
 </html>
 
