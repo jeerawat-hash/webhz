490cf61284e91d3ac135cae4eec3688f0f1211b2
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-18 19:25:41 +0700

    add

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -20,7 +20,7 @@
  
 <body>
 
-	<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+	<nav class="navbar navbar-dark bg-dark navbar-expand-lg">
   <a class="navbar-brand" href="#">ระบบจัดการสมาชิกและและเปลี่ยน</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
