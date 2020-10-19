e8a07335d70242d72a9c52630170e78fe31081aa
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-19 10:32:29 +0700

    add

--- application/views/pages/datafromline.php
+++ application/views/pages/datafromline.php
@@ -166,7 +166,7 @@
 				      <th >ดำเนินการ</th> 
 				    </tr>
 				  </thead>
-				  <tbody id="ChaInvSaleTableContent">
+				  <tbody id="TableInvSaleContent">
 				    
 				  </tbody>
 				  
@@ -381,7 +381,9 @@
 						$("#TableInvSaleContent").html("");
 						//swal("Success !!","เรียกดูข้อมูลช่องเก็บของตัวละครสำเร็จ", "success");
 						var Obj = JSON.parse(data); 
-   						 
+   						
+   						console.log(Obj); 
+   						
    						var tablehtml = "";   
    						for (var i = 0; i < Obj.ChaName.length; i++) {
  
