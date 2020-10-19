143d60983029e432d026c25b822bde3715ec1e1d
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-19 08:47:24 +0700

    add

--- application/views/pages/datafromline.php
+++ application/views/pages/datafromline.php
@@ -351,8 +351,8 @@
 						
 						
 						$("#ChaTableContent").html(tablehtml);
-						
-						$("#UserID").attr('disabled','disabled');
+
+						$("#ChaID").attr('disabled','disabled');
 	 
 					}); 
 
