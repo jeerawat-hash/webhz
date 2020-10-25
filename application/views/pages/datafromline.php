13554c068ca2e0ed75a01154693d5ecd410ce17a
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-10-25 18:07:27 +0700

    add

--- application/views/pages/datafromline.php
+++ application/views/pages/datafromline.php
@@ -243,7 +243,7 @@
 
  
 		
- <div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>
+
 	
 
 
@@ -411,7 +411,7 @@
 									      "<td>Row "+Obj.tab1.X[i]+" Col "+Obj.tab1.Y[i]+"</td>"+
 									      "<td>"+Obj.tab1.ItemName[i]+" <font color='red'>ตีบวก </font>"+enhance+"</td>"+
 									      "<td> <font color='red'>"+ItemQTY+"</font> ชิ้น</td>"+
-									      "<td><button class='btn btn-success isSale' itemqty='"+ItemQTY+"' enhance='"+enhance+"' ItemID='"+Obj.tab1.ItemID[i]+"' itemInfo='"+Obj.tab1.MEM[i]+"'>ฝากขาย</button> <div class='spinner'></div> </td>"+
+									      "<td><button class='btn btn-success isSale' itemqty='"+ItemQTY+"' enhance='"+enhance+"' ItemID='"+Obj.tab1.ItemID[i]+"' itemInfo='"+Obj.tab1.MEM[i]+"'>ฝากขาย</button>  <div class='spinner-border' role='status'><span class='sr-only'>Loading...</span></div> </td>"+
 									    "</tr> "; 
 						}
 
