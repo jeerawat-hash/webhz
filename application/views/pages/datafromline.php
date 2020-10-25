712b12dc6bf89cce6c576999b86ac5dfd0887a26
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-10-25 18:25:18 +0700

    add

--- application/views/pages/datafromline.php
+++ application/views/pages/datafromline.php
@@ -421,7 +421,7 @@
 									      "<td>Row "+Obj.tab1.X[i]+" Col "+Obj.tab1.Y[i]+"</td>"+
 									      "<td>"+Obj.tab1.ItemName[i]+" <font color='red'>ตีบวก </font>"+enhance+"</td>"+
 									      "<td> <font color='red'>"+ItemQTY+"</font> ชิ้น</td>"+
-									      "<td><button class='btn btn-success isSale' itemqty='"+ItemQTY+"' enhance='"+enhance+"' ItemID='"+Obj.tab1.ItemID[i]+"' itemInfo='"+Obj.tab1.MEM[i]+"'>ฝากขาย</button>  <div class='spinner-border text-success spdrawload' role='status'><span class='sr-only'>Loading...</span></div> </td>"+
+									      "<td><button class='btn btn-success isSale' itemqty='"+ItemQTY+"' enhance='"+enhance+"' ItemID='"+Obj.tab1.ItemID[i]+"' itemInfo='"+Obj.tab1.MEM[i]+"'>ฝากขาย</button>   </td>"+
 									    "</tr> "; 
 						}
 
@@ -430,8 +430,7 @@
 						$("#ChaTableContent").html(tablehtml);
 
 						$("#ChaID").attr('disabled','disabled');
-						//////////test
-						$(".spdrawload").hide();
+						 
 	 
 					}); 
 
