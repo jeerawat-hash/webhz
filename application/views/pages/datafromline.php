b4016eb97e38b96ba0efe4f439c43b37a77841e9
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-10-25 19:34:37 +0700

    add

--- application/views/pages/datafromline.php
+++ application/views/pages/datafromline.php
@@ -242,7 +242,28 @@
 
 
  
-		
+	
+	<!-- Modal Sale -->
+	<div class="modal fade" id="SaleItemModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        ...
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
 
 	
 
@@ -746,12 +767,14 @@
 				//alert(IDRAN+" "+ChaID+" "+itemInfo);
 				var Itemqty = $(this).attr("itemqty");
 				
-  
-				$("#ChaTable").find(this).text("กำลังดำเนินการ....");
-				$("#ChaTable").find(this).attr("disabled", true);
 
- 
-				 
+				$("#SaleItemModal").modal("show");
+  
+				//$("#ChaTable").find(this).text("กำลังดำเนินการ....");
+				//$("#ChaTable").find(this).attr("disabled", true);
+
+ 	
+				/*			 
 				$.post("https://cac.webclient.me/api/addItemToShop.php",{
 					ChaNum : ChaID,
 					ItemMEM : itemInfo,
@@ -812,7 +835,7 @@
  
 					
  
-				}); 
+				}); */
 
 
 			});
