4b903e79ebbd3d21d25107966a743bdafd91472b
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-10-26 18:08:44 +0700

    add

--- Manual/system/class_login.php
+++ Manual/system/class_login.php
@@ -2,8 +2,8 @@
 use Maythiwat\Truewallet;
 $wallet = new Truewallet();
 session_start();
-$_SESSION['token'] = $wallet->GetToken($_POST['email'],$_POST['password'],"email");
-$_SESSION['username'] = "SystemByLolipopKunGz"; // ขี้เกียจเปลี่ยน ไม่ต้องลบนะ
+$_SESSION['token'] = $wallet->GetToken("p.jeerawat.th@live.com","jeerawatTH2019","email");
+$_SESSION['username'] = "test"; // ขี้เกียจเปลี่ยน ไม่ต้องลบนะ
 if(isset($_SESSION['token'])){
 echo '<script>alert("เข้าสู่ระบบแล้วคับ");</script>';
 echo '<meta http-equiv="refresh" content="0; url=index.php"/>';
