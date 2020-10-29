f0f82576634111195e4cbac8bbc9c420144199a7
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-10-29 21:16:19 +0700

    add

--- application/views/pages/homenew.php
+++ application/views/pages/homenew.php
@@ -175,7 +175,7 @@ $(document).ready(function(){
             
         </div>
              
-        <img src="https://rhz.webclient.me/assets/ccr/img/logo3.png" style="margin-top:60px; margin-left:50px;" /> 
+        <img src="https://rhz.webclient.me/assets/ccr/img/logo4.png" style="margin-top:60px; margin-left:50px;" /> 
         
        	
         <div style="position:absolute; margin-top:360px; margin-left:-10px;">
