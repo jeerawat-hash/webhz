5fcb0e6e63548a89115fdf826a33a6201ba39236
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-12-26 22:40:41 +0700

    add

--- application/views/pages/itemmall.php
+++ application/views/pages/itemmall.php
@@ -96,10 +96,32 @@ body {
     <div class="card-body p-5">
     
 
+    <div class="row">
+        <div class="col">
+        
+            <div class="card-deck">
+
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <center>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </center>
+                    <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+ 
+            </div>
+        
+        </div>
+    </div>
 
 
-
-
+    
 
 
 
