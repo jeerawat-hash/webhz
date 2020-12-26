77536cf03ee08ae2c523ed68e2a9ebb876d462bc
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-12-26 22:34:16 +0700

    add

--- application/views/pages/itemmall.php
+++ application/views/pages/itemmall.php
@@ -92,8 +92,8 @@ body {
 
 <!-- Page Content -->
 <div class="container">
-  <div class="card border-0 shadow my-8">
-    <div class="card-body p-5">
+  <div class="card border-0 shadow my-5">
+    <div class="card-body p-8">
     
 
 
