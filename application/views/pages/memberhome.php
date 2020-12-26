085d5c94458e63bb08dca7c2c95cdd8553b6959d
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-12-27 00:27:35 +0700

    add

--- application/views/pages/memberhome.php
+++ application/views/pages/memberhome.php
@@ -89,6 +89,59 @@
 
 
 
+
+
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="LoginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">กรุณาเข้าสู่ระบบ</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body"> 
+
+        <div class="container">
+            <div class="row">
+                <div class="col-6" >
+
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">UserID:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+
+                </div>
+                <div class="col-6" >
+                
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Password:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+
+                </div>
+            </div>
+        
+        
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
  
 
 	<script type="text/javascript">
