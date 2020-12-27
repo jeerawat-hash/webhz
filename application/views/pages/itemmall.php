64e7dfe5dbf032e85d00d413182fa0a45be38eb2
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-12-27 16:36:05 +0700

    add

--- application/views/pages/itemmall.php
+++ application/views/pages/itemmall.php
@@ -30,7 +30,7 @@
 <style type="text/css">
  
 body {
-  background: url('https://vistapointe.net/images/ran-online-wallpaper-8.jpg') no-repeat center center fixed;
+  background: url('https://www.wallpaperup.com/uploads/wallpapers/2015/03/20/641752/f3665753b2003cb766707090965ae050.jpg') no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   background-size: cover;
