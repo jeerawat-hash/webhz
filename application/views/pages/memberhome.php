8a1b3547394f820baebaa0fb9ac21154df36b450
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-12-29 02:05:14 +0700

    add

--- application/views/pages/memberhome.php
+++ application/views/pages/memberhome.php
@@ -160,7 +160,7 @@ body {
     <div class="row">
         <div class="col-12"> 
 
-        <div class="jumbotron text-center" style="margin-bottom:0">
+        <div class="jumbotron text-center" style="margin-bottom:0 ; color:white;  background-size: cover; background-image: url('https://www.wallpaperup.com/uploads/wallpapers/2015/03/20/641745/c4f825b2e617ea446f5d5058574ee733.jpg');">
         <h1>ไอเทมแนะนำวันนี้</h1>
         <p>ได้รับส่วนลดพิเศษ30%</p> 
         </div> 
