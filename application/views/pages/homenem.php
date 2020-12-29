b7d6f033ede8cb6a2428ffd6a9196948527a21bd
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-12-29 16:50:28 +0700

    add

--- application/views/pages/homenem.php
+++ application/views/pages/homenem.php
@@ -41,6 +41,7 @@ body {
  
 <body> 
 <!-- Image and text -->
+<!--
 <nav class="navbar navbar-dark bg-dark">
   <a class="navbar-brand" href="https://rhz.webclient.me">
     <img src="https://rhz.webclient.me/assets/bg/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
@@ -64,7 +65,34 @@ body {
  
 
 </nav>
+-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    
+    <a class="navbar-brand" href="https://rhz.webclient.me">
+    <img src="https://rhz.webclient.me/assets/bg/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Hazakura - Ran Online Ep7 Classic
+  </a>
 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav>
 
 
 <div class="container">
