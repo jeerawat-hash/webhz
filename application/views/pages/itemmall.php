ce9f93e8c204ee11d139d00c871a86fbafc112eb
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-12-29 12:27:12 +0700

    add

--- application/views/pages/itemmall.php
+++ application/views/pages/itemmall.php
@@ -221,8 +221,29 @@ body {
       </div>
       <div class="modal-body">
         
+
+        <div class="container">
+          
+          <input type="text" hidden disabled value="" id="ProductNum" >
+
+          <div class="row">
+            <div class="col-12"> <img id="image" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22895%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20895%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_176acf1eb7e%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A45pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_176acf1eb7e%22%3E%3Crect%20width%3D%22895%22%20height%3D%22250%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22332.390625%22%20y%3D%22145.165625%22%3E895x250%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="img-fluid" alt="Responsive image"> </div> 
+          </div>
+
+          <div class="row">
+            <div class="col-6">ราคา :</div> 
+            <div class="col-6"> <font color="red" id="price"></font>  พ้อย</div> 
+          </div>
+          <div class="row">
+            <div class="col-2">รายละเอียด :</div> 
+            <div class="col-10"> <font color="red" id="desc"></font>  พ้อย</div> 
+          </div>
+ 
+
+        </div>
+
+
       
-      <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22895%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20895%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_176acf1eb7e%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A45pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_176acf1eb7e%22%3E%3Crect%20width%3D%22895%22%20height%3D%22250%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22332.390625%22%20y%3D%22145.165625%22%3E895x250%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="img-fluid" alt="Responsive image">
 
       
 
