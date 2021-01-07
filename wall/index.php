b519f9da0fc2d9e1b3c853564fd15ce961689c96
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-01-08 00:22:23 +0700

    add

--- wall/index.php
+++ wall/index.php
@@ -4,9 +4,15 @@
 
   $tw->setAccessToken("766a68c1-a9ea-4983-b37d-8508e23c5741");
   $data = $tw->GetTransaction();
+  $data1 = $tw->GetTransactionReport("50006595955747");
+
+
+  print_r($data1);
+
+  /*
   foreach ($data["data"]["activities"] as $transfer) {
     $values = $tw->GetTransactionReport($transfer["report_id"]);
     print_r($values);
-  }
+  }*/
   
 ?>
