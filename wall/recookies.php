c5b1a5090092bf433cefe14046ed278aa00eb8b9
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-01-08 00:13:09 +0700

    add

--- wall/recookies.php
+++ wall/recookies.php
@@ -1,7 +1,7 @@
 <?php
   require "TrueWalletClass.php"; 
   $tw = new TrueWalletClass("0616619956", "jeerawatTH2016");
-  $tw->setAccessToken($access_token);
+  $tw->setAccessToken("766a68c1-a9ea-4983-b37d-8508e23c5741");
   $data = $tw->GetProfile();
   print($data["code"]);
  
