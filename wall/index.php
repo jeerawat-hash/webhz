c887a3088cbaffa7494856b0a10fb31af3e041e6
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-01-07 23:46:30 +0700

    add

--- wall/index.php
+++ wall/index.php
@@ -1,12 +1,12 @@
-<?php
-	ini_set('display_errors', 1);
-	include_once('manager/TrueWallet.php');
+<?php 
+	include './manager/TrueWallet.php';
 	$wallet = new TrueWallet(); 
 
 	$a = $wallet->GetToken("p.jeerawat.th@live.com","jeerawatTH2016","email");
 	 
 
 	echo $a;
+	var_dump($a);
 
 
 
