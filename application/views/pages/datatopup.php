c7a6ab9f66434438da66387c0f28158eaa01c4e6
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-01-08 18:55:02 +0700

    add

--- application/views/pages/datatopup.php
+++ application/views/pages/datatopup.php
@@ -35,7 +35,7 @@
 <body>
 
 	<nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-	<a class="navbar-brand" href="https://rhz.webclient.me/index.php/member">หน้าแรก</a>
+	<a class="navbar-brand" href="https://rhz.webclient.me/index.php/member">จัดการตัวละคร</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
